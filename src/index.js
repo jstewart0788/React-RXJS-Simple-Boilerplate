1b98bd35d6b1abe43f32f60287e56ab4b9608334
Jonathan Stewart <jstewart0788@gmail.com>
2016-12-06 20:02:28 -0500

    through first section of Rx tutorial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import {Observable} from 'rxjs'
+
+let source = Observable.fromEvent(document, "mousemove")
+  .map(e => {
+    return{
+      x:e.clientX,
+      y:e.clientY
+    }
+  })
+  .filter(value=> value.x < 500);
+
+source.subscribe(
+  value => console.log(value),
+  e => console.log(`Error: ${e}`),
+  () => console.log('Complete')
+);
 
 const App = () => {
   return(
   <div>
-    <h1> Hey Test!</h1>
+    <h1> Hello World!</h1>
   </div>
   );
 };
